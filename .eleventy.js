df5bdebdc4e5098d4b1a202aa5062967d4758f48
willywongi <pongi@pongi.it>
2020-08-17 11:40:56 +0200

    Corretti i commenti e la formattazione.

--- .eleventy.js
+++ .eleventy.js
@@ -1,12 +1,15 @@
 module.exports = function(eleventyConfig) {
-    // Output directory: _site
-  
-    eleventyConfig.addPassthroughCopy("visual");
+  // Come servire risorse statiche
+  // https://www.11ty.dev/docs/copy/#manual-passthrough-file-copy-(faster)
+  eleventyConfig.addPassthroughCopy("visual");
 
-    eleventyConfig.addCollection("featuredActivity", function(collectionApi) {
-      let now = new Date();
-      let month = `${now.getMonth() + 1}`.padStart(2, "0");
-      let featuredTag = `featured${now.getFullYear()}${month}`;
-      return collectionApi.getFilteredByTags(featuredTag).slice(0, 3);
-    });
-  };
+  // Questa collezione custom prende i documenti che hanno il tag 
+  // relativo al mese corrente (es.: `featured202008`) e ne tiene 
+  // solo i primi 3.
+  eleventyConfig.addCollection("featuredActivity", function(collectionApi) {
+    let now = new Date();
+    let month = `${now.getMonth() + 1}`.padStart(2, "0");
+    let featuredTag = `featured${now.getFullYear()}${month}`;
+    return collectionApi.getFilteredByTags(featuredTag).slice(0, 3);
+  });
+};
